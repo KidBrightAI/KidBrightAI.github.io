8e070f42e060cfe4573f3ac28b04735b0552d5a2
comdet <listzone@hotmail.com>
2023-07-25 04:40:19 +0700

    add hand, pose, facelandmark

---
+++ _nuxt/d5969ae.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{1157:function(n,e,d){"use strict";d.r(e);var l=d(12),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
